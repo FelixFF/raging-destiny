d59577952117aaceff26ffcff52324de1aa58f50
Felix <felix@buildaviator.services>
2021-05-22 22:54:25 +0100

    setting up app

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="content"><span>Coming Soon</span><span class="one">.</span><span class="two">.</span><span class="three">.</span></div>
+
+@endsection
